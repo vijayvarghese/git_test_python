eed7a84a7b4bbb21114eb2c0a0423433b9df9178
Vijay Varghese <Vijayvarghese2016@gmail.com>
2024-01-15 03:13:45 -0500

    add function added, also prompt for a number

--- test.py
+++ test.py
@@ -2,12 +2,20 @@ a = str()
 def Test(a):
     print("Hello, ",a)
 
+def add(b):
+    a = int(b)+10
+    print("\n","Number : ",a)
 
 def get():
     item = input("Enter Name : ")
+    return item
+def get2():
+    item = input("Enter a Number : ")
     return item
 
 if __name__ == "__main__":
     
     Test(get())
+    add(get2())
+
 
